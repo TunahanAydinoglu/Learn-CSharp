15a7281536183d4fc265b13f4a85245164aa248c
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-08-24 17:12:23 +0300

    RecapProject1

---
+++ RecapProject1/RecapProject1/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecapProject1.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ RecapProject1/RecapProject1/NorthwindContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecapProject1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecapProject1
+{
+    public class NorthwindContext:DbContext
+    {
+        
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Northwind;Integrated Security=True");
+        }
+    }
+}
