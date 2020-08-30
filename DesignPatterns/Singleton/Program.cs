f654e571cc57b78d72801c141890806d5d18440b
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-08-30 17:23:07 +0300

    Singleton

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+        }
+    }
+
+    class CustomerManager
+    {
+        static CustomerManager _customerManager;
+        private CustomerManager()
+        {
+
+        }
+        //public static CustomerManager CreateAsSingleton()
+        //{
+        //    if(_customerManager == null)
+        //    {
+        //        _customerManager = new CustomerManager();
+        //    }
+        //    return _customerManager;
+        //}
+        public static CustomerManager CreateAsSingleton()
+        {
+            return _customerManager ?? (_customerManager = new CustomerManager());
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved Singleton");
+        }
+    }
+}
