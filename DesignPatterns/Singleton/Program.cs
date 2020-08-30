3d54617daab74a8ce4235d7989b5079015e6358c
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-08-30 17:29:38 +0300

    ThreadSafeSingleton

--- DesignPatterns/Singleton/Program.cs
+++ DesignPatterns/Singleton/Program.cs
@@ -13,10 +13,27 @@ namespace Singleton
 
     class CustomerManager
     {
-        static CustomerManager _customerManager;
+        private static CustomerManager _customerManager;
+        static object _lockObject = new object();
+
         private CustomerManager()
         {
 
+        }
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock (_lockObject) { 
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+                return _customerManager;
+            }
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved Singleton");
         }
         //public static CustomerManager CreateAsSingleton()
         //{
@@ -26,14 +43,6 @@ namespace Singleton
         //    }
         //    return _customerManager;
         //}
-        public static CustomerManager CreateAsSingleton()
-        {
-            return _customerManager ?? (_customerManager = new CustomerManager());
-        }
 
-        public void Save()
-        {
-            Console.WriteLine("Saved Singleton");
-        }
     }
 }
