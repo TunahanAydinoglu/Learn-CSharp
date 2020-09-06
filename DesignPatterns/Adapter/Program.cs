9749c05f61e18c324cd8fa11285fbb86c80fb480
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-09-06 14:18:49 +0300

    A

---
+++ DesignPatterns/Adapter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class ProductManager
+    {
+        private ILogger _logger;
+        public ProductManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Save()
+        {
+            _logger.Log("User Data");
+            Console.WriteLine("Saved");
+        }
+    }
+    interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class TaLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"Ta Logger Message : {message}");
+        }
+    }
+}
